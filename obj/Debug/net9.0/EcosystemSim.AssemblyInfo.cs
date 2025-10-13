7db1b751e4c51a4d05672361f7f9c9ddcada4edf
mightygryphon999 <griffin.roberts.srq@gmail.com>
2025-10-13 17:30:33 -0400

    Update build outputs and assembly version
    Regenerated build artifacts and updated AssemblyInformationalVersion to reflect latest source changes. No source code modifications included.

--- obj/Debug/net9.0/EcosystemSim.AssemblyInfo.cs
+++ obj/Debug/net9.0/EcosystemSim.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcosystemSim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bf451a8b2e10c8d22825ea87c46c891f2145c5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67153ca106c125a78857c76ae208898303abf6a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcosystemSim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcosystemSim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
