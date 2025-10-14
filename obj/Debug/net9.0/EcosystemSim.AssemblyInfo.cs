d2e5e9ff0db207117c09b149dd515551a4364265
mightygryphon999 <griffin.roberts.srq@gmail.com>
2025-10-13 21:21:49 -0400

    update hunger and thrist conditions

--- obj/Debug/net9.0/EcosystemSim.AssemblyInfo.cs
+++ obj/Debug/net9.0/EcosystemSim.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcosystemSim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60511df0ce016ee5826e7012b2066849b97544df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e22b0dafe06c5325eff0ab2d12cd426347252029")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcosystemSim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcosystemSim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
