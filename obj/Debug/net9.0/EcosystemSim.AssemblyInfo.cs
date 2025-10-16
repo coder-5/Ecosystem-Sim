a758aa4a13ed62ee4a9035e200a5f6ff8f762a91
mightygryphon999 <griffin.roberts.srq@gmail.com>
2025-10-16 12:20:39 -0400

    fixed the ui bugs and did another build

--- obj/Debug/net9.0/EcosystemSim.AssemblyInfo.cs
+++ obj/Debug/net9.0/EcosystemSim.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcosystemSim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8296abf7365697981a38754f04f87b502d3a26c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30215039fa14aa91f7de97f1bba5a20a8e89bd96")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcosystemSim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcosystemSim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
