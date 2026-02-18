2bfeb74943b5ebfc5822f3e7365bf4b4e785a664
mightygryphon999 <griffin.roberts.srq@gmail.com>
2026-02-17 22:11:26 -0500

    Bump AssemblyInformationalVersion
    Update AssemblyInformationalVersion in obj/Debug/net9.0/EcosystemSim.AssemblyInfo.cs from 1.0.0+cf0a10e8fbf8d5a29134b8bbe0dc017d6aa9d10d to 1.0.0+09a5503ba2068fd60cdb9a89cac94d092a0a611a and refresh the AssemblyInfoInputs.cache hash. These are autogenerated debug build artifacts reflecting updated build metadata.

--- obj/Debug/net9.0/EcosystemSim.AssemblyInfo.cs
+++ obj/Debug/net9.0/EcosystemSim.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcosystemSim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf0a10e8fbf8d5a29134b8bbe0dc017d6aa9d10d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09a5503ba2068fd60cdb9a89cac94d092a0a611a")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcosystemSim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcosystemSim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
