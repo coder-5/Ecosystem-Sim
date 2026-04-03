fb38e039357077faece08e9f0aa0b1dc74f12ca6
coder-5 <vihaan.agrawalcoder@gmail.com>
2026-04-03 11:33:30 -0400

    Update file paths and add `WaterZone` class for Java ecosystem simulation

---
+++ Java/src/WaterZone.java
@@ -0,0 +1,11 @@
+public class WaterZone {
+        public double amountOfWater;
+        public double xPos;
+        public double yPos;
+
+        public WaterZone(int amountOfWater, int xPos, int yPos) {
+                this.amountOfWater = amountOfWater;
+                this.xPos = xPos;
+                this.yPos = yPos;
+        }
+}
